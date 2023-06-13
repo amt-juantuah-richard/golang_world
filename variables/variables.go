58a2ab3d4ebe3610b77f2c41abed0368d49a27c1
mrbridge-dev <mrbridge-dev@penguin>
2023-06-13 07:51:59 +0000

    mod: variables

---
+++ variables/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myName string = "Jigjigga"
+	var country, city = "Ghana", "Accra"
+	var age = 99
+	religion := "X-men"
+
+	fmt.Println(myName, country, city, age, religion)
+}
